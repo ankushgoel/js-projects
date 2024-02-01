2b9a03ef91030756c90ef6283d519d4b06c1fadc
Ankush Goel <goelankush1312@gmail.com>
2024-02-01 23:34:52 +0530

    P6-Unlimited BG Colors script added

--- 6-UnlimitedBG/script.js
+++ 6-UnlimitedBG/script.js
@@ -1,1 +1,24 @@
-// 
+const randomColor = function () {
+  const hex = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += hex[parseInt(Math.random() * 16)];
+  }
+  return color;
+};
+
+let changeColorInterval;
+const startChangingColor = function () {
+  changeColorInterval = setInterval(function () {
+    document.body.style.backgroundColor = randomColor();
+  }, 1000);
+};
+
+const stopChangingColor = function () {
+  clearInterval(changeColorInterval);
+  changeColorInterval = null;
+};
+
+document.querySelector('#start').addEventListener('click', startChangingColor);
+
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
